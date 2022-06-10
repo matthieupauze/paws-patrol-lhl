caf4feed22817eca23f5bb51fd9feb91b85a208e
firewallfail <firewall.fail@hotmail.com>
2022-06-09 21:51:56 -0400

    connect to db using sequelize

--- server/db/db.js
+++ server/db/db.js
@@ -1,4 +1,16 @@
+require("dotenv").config();
 
+const { DB_HOST, DB_USER, DB_PORT, DB_PASSWORD, DB_NAME } = process.env;
 
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname')
+
+const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+  host: DB_HOST,
+  dialect: 'postgres'
+});
+
+sequelize.authenticate().then(() => {
+  console.log("Success!");
+}).catch((err) => {
+  console.log(err);
+});
