bbac5231bc349fc7f441b564a030b350776c7f71
Matthieu Pauze <matt.pauze@gmail.com>
2022-06-12 16:12:23 -0400

    Doc: Format code using prettier

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,7 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-const apiRouter = require('./routes/api')
+const apiRouter = require('./routes/api');
 
 var app = express();
 
