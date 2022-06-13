aec58a447be17e7dcc76d9f340f093f5234814aa
Robbie Prokop <iuliiaprokop@Robbies-MacBook-Pro.local>
2022-06-13 11:31:30 -0600

    some pushy

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,56 +1,30 @@
-import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
-
-// const getCurrentUserLocation = (map) => {
-//   navigator.geolocation.getCurrentPosition((position) => {
-//     const latitude = position.coords.latitude;
-//     const longitude = position.coords.longitude;
-//     map.setView([latitude, longitude], 15);
-//   });
-//   $.get("/home");
-// };
-
-// const loadMap = function () {
-//   const map = L.map("map", {
-//     doubleClickZoom: false,
-//     bubblingMouseEvents: true,
-//     zoomControl: false,
-//   }).setView([49.262838, -122.781071], 16);
-
-//   L.control
-//     .scale({
-//       metric: true,
-//       imperial: false,
-//       position: "bottomright",
-//     })
-//     .addTo(map);
-//   L.control
-//     .zoom({
-//       position: "bottomright",
-//     })
-//     .addTo(map);
-
-//   getCurrentUserLocation(map);
-//   // Posting new map title,lat,long to server
-
-//   return map;
-// };
-// const renderMap = function (map) {
-//   L.tileLayer(
-//     "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
-//     {
-//       attribution:
-//         'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-//       maxZoom: 18,
-//       id: "mapbox/streets-v11",
-//       tileSize: 512,
-//       zoomOffset: -1,
-//       accessToken:
-//         "pk.eyJ1IjoiZGtlbGw4OCIsImEiOiJjbDJ3Zm44NjMwZjVqM2RxY3gyN3J6dXJ2In0.SYE3QdtfFxH63YvUTI7FMA",
-//     }
-//   ).addTo(map);
-// };
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMapEvents,
+} from "react-leaflet";
+import { useEffect, useRef, useState } from "react";
 
 function App() {
+  const Location = () => {
+    const [position, setPosition] = useState(null);
+    const map = useMapEvents({
+      click() {
+        map.locate();
+      },
+      locationfound(e) {
+        const lat = e.latlng.lat;
+        const lng = e.latlng.lng;
+        setPosition(e.latlng);
+        map.flyTo(e.latlng, map.getZoom());
+      },
+    });
+    return position === null ? null : <Marker position={position}></Marker>;
+  };
+  const position = 
+
   return (
     <MapContainer
       center={[51.049999, -114.066666]}
@@ -61,11 +35,7 @@ function App() {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[51.049999, -114.066666]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+      <Location />,
     </MapContainer>
   );
 }
