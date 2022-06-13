fb34f438641ae5678c1bb7f9ff76b777ec573063
Matthieu Pauze <matt.pauze@gmail.com>
2022-06-13 15:10:59 -0400

    Feature: Implement login page with CSS. Style tbd

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,24 +1,26 @@
+import './Login.css'
+
 export const Login = () => {
   return (
-    <section className="d-flex justify-content-center vh-100">
-      <form className="align-items-center">
-        <div className="form-group my-3">
+    <section className="login d-flex justify-content-center align-items-center vh-100 flex-column">
+      <form className="d-flex flex-column justify-content-center h-25 w-50">
+        <div className="form-group my-3 p-3 ">
           <input
-            className="form-control"
+            className="form-control bg-transparent text-white ph-color"
             type="email"
             id="email"
             placeholder="Email"
           />
         </div>
-        <div className="form-group my-3">
+        <div className="form-group mb-3 p-3">
           <input
-            className="form-control"
+            className="form-control form-control bg-transparent text-white ph-color"
             type="password"
             id="password"
             placeholder="Password"
           />
         </div>
-        <button type="submit" className="btn btn-primary">
+        <button type="submit" className="btn btn-primary m-3">
           Login
         </button>
       </form>
