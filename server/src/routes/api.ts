f9502828f13c73dab000508121268038e809fed6
firewallfail <firewall.fail@hotmail.com>
2022-06-13 15:13:19 -0400

    Try to make prettier happy

--- server/src/routes/api.ts
+++ server/src/routes/api.ts
@@ -1,3 +1,4 @@
+// Api Controller
 module.exports = (router: any, db: any) => {
 
   // Get route to reset DB
@@ -10,6 +11,7 @@ module.exports = (router: any, db: any) => {
     res.send("Nothing to see here!");
   });
 
+  // Add coordinates to db
   router.post('/:imei', (req: any, res: any) => {
     const { imei } = req.params;
     const { lat, long, time } = req.body;
