b45566fb64e50123b57b98d183beb51fe6e363d4
firewallfail <firewall.fail@hotmail.com>
2022-06-14 10:03:51 -0400

    move coordinate to coordinate route, remove old api routing

--- server/src/routes/coordinateApi.ts
+++ server/src/routes/coordinateApi.ts
@@ -1,5 +1,17 @@
 module.exports = (router: any, db: any) => {
-  router.get('/', (req: any, res: any) => {
-    res.send('coordinate');
-  });
+    // Get most recent coord from db for specified imei
+    router.get('/:imei', (req: any, res: any) => {
+      const { imei } = req.params;
+      db.getCoordinate(Number(imei))
+      .then((data: any) => res.send(data.dataValues))
+      .catch((err: any) => res.send(err));
+    });
+  
+    // Add coordinates to db
+    router.post('/:imei', (req: any, res: any) => {
+      const { imei } = req.params;
+      const { lat, long, time } = req.body;
+      db.addCoordinates(Number(imei), lat, long, time);
+      res.send(`imei: ${imei} lat: ${lat} long: ${long} time: ${time}\n`);
+    });
 };
