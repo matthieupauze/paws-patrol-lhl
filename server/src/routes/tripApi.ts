991b9d486b678c50539b0b3abf94fc61b537ca2e
firewallfail <firewall.fail@hotmail.com>
2022-06-14 16:22:31 -0400

    Lay out routes for trip api

--- server/src/routes/tripApi.ts
+++ server/src/routes/tripApi.ts
@@ -1,6 +1,22 @@
 module.exports = (router: any, db: any) => {
+  // send all trips
   router.get('/', (req: any, res: any) => {
     res.send('trip');
+  });
+
+  // add a new trip using start time
+  router.post('/:imei', (req: any, res: any) => {
+    res.send('start trip');
+  });
+
+  // get all trips for 1 device
+  router.get('/:imei', (req: any, res: any) => {
+    res.send('get trip');
+  });
+
+  // end a trip with end time
+  router.patch('/:imei', (req: any, res: any) => {
+    res.send('finish trip');
   });
 
   return router;
