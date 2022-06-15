6c4a9c21e3f034f7c7420799e518f22416aea7a2
firewallfail <firewallfail@users.noreply.github.com>
2022-06-15 20:34:55 +0000

    Prettified Code!

--- server/src/routes/perimeterApi.ts
+++ server/src/routes/perimeterApi.ts
@@ -2,11 +2,11 @@ module.exports = (router: any, db: any) => {
   // Get all perimeters
   router.get('/', (req: any, res: any) => {
     db.getPerimeters()
-    .then((data: any) => res.status(200).json(data))
-    .catch((err: any) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+      .then((data: any) => res.status(200).json(data))
+      .catch((err: any) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
   });
 
   // Add a new perimeter based on imei
@@ -14,21 +14,21 @@ module.exports = (router: any, db: any) => {
     const { imei } = req.params;
     const { left, right } = req.body;
     db.addPerimeter(imei, left, right)
-    .then((data: any) => res.status(200).json(data))
-    .catch((err: any) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+      .then((data: any) => res.status(200).json(data))
+      .catch((err: any) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
   });
 
   // Get one perimeter based on imei
   router.get('/:imei', (req: any, res: any) => {
     db.getPerimeterByIMEI(req.params.imei)
-    .then((data: any) => res.status(200).json(data))
-    .catch((err: any) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+      .then((data: any) => res.status(200).json(data))
+      .catch((err: any) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
   });
 
   // Update one perimeter based on imei
@@ -36,14 +36,14 @@ module.exports = (router: any, db: any) => {
     const { imei } = req.params;
     const { left, right } = req.body;
     db.updatePerimeter(imei, left, right)
-    .then((data: any) => {
-      console.log(data);
-      res.status(200).json(data);
-    })
-    .catch((err: any) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+      .then((data: any) => {
+        console.log(data);
+        res.status(200).json(data);
+      })
+      .catch((err: any) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
   });
 
   return router;
