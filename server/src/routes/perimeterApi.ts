a1815c80016c4a0ddc0d0417576f699c36ba3b29
firewallfail <firewall.fail@hotmail.com>
2022-06-15 11:13:34 -0400

    Create templates for perimeter routes

--- server/src/routes/perimeterApi.ts
+++ server/src/routes/perimeterApi.ts
@@ -1,6 +1,18 @@
 module.exports = (router: any, db: any) => {
   router.get('/', (req: any, res: any) => {
-    res.send('perimeter');
+    res.send('all perimeters');
+  });
+
+  router.post('/:imei', (req: any, res: any) => {
+    res.send('add perimeter')
+  });
+
+  router.get('/:imei', (req: any, res: any) => {
+    res.send('one perimeter');
+  });
+
+  router.patch('/:imei', (req: any, res: any) => {
+    res.send('updated perimeter');
   });
 
   return router;
