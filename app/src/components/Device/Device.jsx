fb5e0c13ebbbbe0314b9a3c194e8100572df2f80
Robbie Prokop <iuliiaprokop@Robbies-MacBook-Pro.local>
2022-06-14 20:36:06 -0600

    updated with proper merge from frontend to app

--- app/src/components/Device/Device.jsx
+++ app/src/components/Device/Device.jsx
@@ -1,14 +1,46 @@
-import { useState } from 'react';
-import { ListGroup, ListGroupItem, Form, Button, Card } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { useState, useEffect } from "react";
+import { ListGroup, ListGroupItem, Form, Button, Card, ButtonGroup, ToggleButton } from "react-bootstrap";
+import { Link } from "react-router-dom"
+import axios from "axios"
 
-function Device() {
-  const [devices, setDevices] = useState([]);
+const Device = () => {
+  const [form, setForm] = useState(false)
+  const [devices, setDevices] = useState([
+    // {name: "Device 1", imei: "1"}, 
+    // {name: "Device 2", imei: "2"}
+  ]);
+  const [radioImei, setRadioImei] = useState("");
+  const radios = devices
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const newDevice = {
+      name: e.target[0].value, 
+      imei: e.target[1].value, 
+      microchip: e.target[2].value
+    }
+    const newDevices = [...devices, newDevice]
+    setDevices(newDevices)
+    axios.post(`/api/device/${newDevice.imei}`, newDevice)
+      .then(res => setForm(!form), console.log("device uploaded"))
+      .catch(err => console.log(err))
+  }
+
+  useEffect(() => {
+    const loadDevices = async () => {
+      const { data } = await axios.get("/api/device")
+      setDevices(data);
+    };
+    loadDevices();
+  }, []);
+
   return (
     <>
-      {devices.length === 0 && (
-        <section className="login d-flex justify-content-center align-items-center flex-column">
-          <Form className="d-flex flex-column justify-content-center h-25 w-50">
+    {form &&
+      <section className="login d-flex justify-content-center align-items-center flex-column">
+        <Card className="m-3 p-3 rounded w-25 ph-color">
+
+          <Form className="d-flex flex-column justify-content-center " onSubmit={handleSubmit}>
             <Form.Group className="form-group my-2 p-3">
               <Form.Control
                 className="form-control bg-transparent text-white ph-color"
@@ -19,63 +51,75 @@ function Device() {
             </Form.Group>
             <Form.Group className="form-group my-2 pb-3 px-3">
               <Form.Control
-                className="form-control bg-transparent text-white ph-color"
-                type="text"
-                id="pet-name"
-                placeholder="Pet Name"
-              />
-            </Form.Group>
-            <Form.Group className="form-group my-2 pb-3 px-3">
-              <Form.Control
-                className="form-control bg-transparent text-white ph-color"
-                type="number"
-                id="microchip"
-                placeholder="Microchip Number"
-              />
-            </Form.Group>
-            <Form.Group className="form-group my-2 pb-3 px-3">
-              <Form.Control
                 className="form-control form-control bg-transparent text-white ph-color"
-                type="number"
+                type="text"
                 id="imei"
                 placeholder="Device IMEI"
               />
             </Form.Group>
-            <div className=" mt-2 px-3">
-              <Link to="/">
-                <Button type="submit" className="btn-color rounded w-100">
+            <Form.Group className="form-group my-2 pb-3 px-3">
+              <Form.Control
+                className="form-control bg-transparent text-white ph-color"
+                type="text"
+                id="microchip"
+                placeholder="Microchip Number"
+              />
+            </Form.Group>
+            <Form.Group className=" my-2 px-3">
+                <Button 
+                type="submit" 
+                className="btn-color rounded w-100"
+                >
                   Add Device
                 </Button>
-              </Link>
-            </div>
+              
+            </Form.Group>
           </Form>
-        </section>
-      )}
-      {devices.length !== 0 && (
-        <section className="login d-flex justify-content-center align-items-center flex-column">
-          <Card className="my-3 py-3 rounded">
-            <ListGroup>
-              <ListGroupItem>Device 1</ListGroupItem>
-              <ListGroupItem>Device 2</ListGroupItem>
-              <ListGroupItem>Device 3</ListGroupItem>
-            </ListGroup>
-            <div className="d-grid gap-2">
-              <Link to="/">
-                <Button type="submit" className="btn-color rounded w-100 ">
-                  Add Device
-                </Button>
-              </Link>
-              <Link to="/">
-                <Button type="submit" className="btn-color rounded w-100">
-                  Edit Device
-                </Button>
-              </Link>
-            </div>
-          </Card>
-        </section>
-      )}
+        </Card>
+      </section>
+    }
+    {!form &&
+      <section className="login d-flex justify-content-center align-items-center flex-column">
+        <Card className="m-3 p-3 rounded w-25 ph-color">
+
+          <ButtonGroup className="mb-3 d-flex flex-column gap-2 rounded">
+            {radios.map((radio, idx) => (
+              <ToggleButton
+               key={idx}
+               id={`radio-${idx}`}
+               type="radio"
+               name="radio"
+               value={radio.imei}
+               checked={radioImei === radio.imei}
+               variant={radioImei === radio.imei ? "secondary" : "transparent"}
+               onChange={(e) => setRadioImei(e.currentTarget.value)}
+              >
+                {radio.name}
+              </ToggleButton>
+              // react initial render get request api to get all the devices useeffect to set Devices to all the devices available in the db
+            ))}
+          </ButtonGroup>
+          <div className="d-grid gap-3">
+
+            <Link to='/device'>
+              <Button 
+                type="submit" 
+                className="btn-color rounded w-100" 
+                onClick={(e) => {setForm(!form)}}>
+                Add Device
+              </Button>
+            </Link>
+            <Link to='/device'>
+              <Button type="submit" className="btn-color rounded w-100">
+                Edit Device
+              </Button>
+            </Link>
+          </div>
+        </Card>
+      </section>
+    }
     </>
   );
-}
+};
 
-export default Device;
+export default Device
