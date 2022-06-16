0cf8e92e8e4c202b182701c5b7ba6f571485ecb8
Matthieu Pauze <matt.pauze@gmail.com>
2022-06-16 00:16:51 -0400

    Configure vite to launch on environment defined port

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,8 +1,16 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
+import path from 'node:path';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, path.resolve(process.cwd(), '../'), '');
+  return {
+    plugins: [react()],
+    server: {
+      port: env.REACT_PORT,
+      strictPort: true,
+    },
+  };
 });
