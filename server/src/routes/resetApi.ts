0bc4532c17bcfbe9c958b7fcf6b3f14c49f08abc
firewallfail <firewall.fail@hotmail.com>
2022-06-16 17:58:45 -0400

    Route to reset and seed

--- server/src/routes/resetApi.ts
+++ server/src/routes/resetApi.ts
@@ -9,5 +9,15 @@ module.exports = (router: any, db: any) => {
       });
   });
 
+  // Get route to reset and seed DB
+  router.get('/seed', (req: any, res: any) => {
+    db.seedDB()
+      .then(() => res.send('DB Reset and seed'))
+      .catch((err: any) => {
+        console.log(err);
+        res.status(500).json('An error occured, DB not reset');
+      });
+  });
+
   return router;
 };
