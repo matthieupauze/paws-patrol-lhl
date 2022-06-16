f963cd0d49f7107467d7247259caf4729ab29067
firewallfail <firewall.fail@hotmail.com>
2022-06-16 18:31:58 -0400

    remove unnecessary route

--- server/src/routes/emailApi.ts
+++ server/src/routes/emailApi.ts
@@ -1,9 +1,5 @@
 module.exports = (router: any, db: any) => {
 
-  router.get('/', (req: any, res: any) => {
-    res.send("brokey");
-  });
-
   router.post('/', (req: any, res: any) => {
     res.send("brokey?");
   });
