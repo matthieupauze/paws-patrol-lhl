e8153eef2f1aea578b00eff4564b37125512b9a9
firewallfail <firewall.fail@hotmail.com>
2022-06-17 11:33:20 -0400

    Sendinblue template

---
+++ server/src/routes/helpers/sendMail.ts
@@ -0,0 +1,36 @@
+require("dotenv").config();
+
+const { EMAIL, PASS } = process.env;
+const nodemailer = require("nodemailer");
+const path = require("path");
+
+const sendEmail = async (to: string, subject: string, text: string) => {
+  try {
+    let transporter = nodemailer.createTransport({
+      host: "smtp-relay.sendinblue.com",
+      port: 587,
+      auth: {
+        user: EMAIL,
+        pass: PASS,
+      },
+    });
+
+    let info = await transporter.sendMail({
+      from: EMAIL,
+      to: to,
+      subject: subject,
+      text: text,
+    })
+
+    console.log(`Message send: ${info.messageId}`);
+    return `Message send: ${info.messageId}`;
+
+  } catch (error: any) {
+    console.error(error);
+    throw new Error(
+      `Something went wrong in the sendmail method. Error: ${error.message}`
+    );
+  }
+};
+
+module.exports = sendEmail;
