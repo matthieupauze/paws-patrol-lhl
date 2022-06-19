f5fcd015267238c2fe99b970d2352096b678eb39
Matthieu Pauze <matt.pauze@gmail.com>
2022-06-19 17:36:24 -0400

    Remove duplicate code in view changing button

--- app/src/components/Map/Map.jsx
+++ app/src/components/Map/Map.jsx
@@ -6,7 +6,7 @@ import usePoll from '../../hooks/usePoll';
 import useTracker from '../../hooks/useTracker';
 import useEvent from '../../hooks/useEvent';
 
-// const { VITE_PORT_EXPRESS } = import.meta.env;
+const { VITE_PORT_EXPRESS } = import.meta.env;
 
 const defaultZoom = 4;
 const trackingZoom = 18;
@@ -14,11 +14,13 @@ const defaultPosition = { imei: 0, lat: 45, lng: -73 };
 const streetData = {
   url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  iconURL: 'images/street.png',
 };
 const satelliteData = {
   url: 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
   attribution:
     'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
+  iconURL: 'images/earth.jpeg',
 };
 
 function Tracker() {
@@ -59,13 +61,6 @@ function Map({ interactive, perimeter, setPerimeters, setActive, track }) {
   const [satelliteView, setSatelliteView] = useState(false);
   const [tileLayerData, setTileLayerData] = useState(streetData);
 
-  const savePerimeter = () => {
-    const data = { p1lat: p1.lat, p1long: p1.lng, p2lat: p2.lat, p2long: p2.lng };
-    axios
-      .post(`http://localhost:${VITE_PORT_EXPRESS}/api/perimeter/1`, data)
-      .then((res) => setActive && setActive(false))
-      .catch((err) => console.log('err', err.message));
-  };
   useEffect(() => {
     const loadPerimeters = async () => {
       const { data } = await axios.get(`http://localhost:${VITE_PORT_EXPRESS}/api/perimeter`);
@@ -74,22 +69,20 @@ function Map({ interactive, perimeter, setPerimeters, setActive, track }) {
     loadPerimeters();
   }, []);
 
-  const startTracking = () => {
-    // CHANGE THE IMEI !!!
-    const data = { start: Date.now() };
+  const savePerimeter = () => {
+    const data = { p1lat: p1.lat, p1long: p1.lng, p2lat: p2.lat, p2long: p2.lng };
     axios
-      .post(`http://localhost:${VITE_PORT_EXPRESS}/api/trip/1`, data)
-      .then((res) => {
-        setTracking(true);
-      })
+      .post(`http://localhost:${VITE_PORT_EXPRESS}/api/perimeter/1`, data)
+      .then(() => setActive && setActive(false))
       .catch((err) => console.log('err', err.message));
   };
-  const stopTracking = () => {
+
+  const toggleTracking = (toggle) => {
     const data = { end: Date.now() };
     axios
       .patch(`http://localhost:${VITE_PORT_EXPRESS}/api/trip/1`, data)
-      .then((res) => {
-        setTracking(false);
+      .then(() => {
+        setTracking(toggle);
       })
       .catch((err) => console.log('err', err.message));
   };
@@ -133,31 +126,16 @@ function Map({ interactive, perimeter, setPerimeters, setActive, track }) {
             </div>
           )}
           {/* satelite view button */}
-          {!satelliteView && (
-            <div className="info w-25 align-self-start m-3 pb-4">
-              <Card className=" w-25 rounded ph-color contain">
-                <div className="d-grid">
-                  <Button
-                    className="satelite-view rounded w-100"
-                    onClick={(e) => setSatellite(!satelliteView)}
-                  />
-                </div>
-              </Card>
-            </div>
-          )}
-          {/* street view button */}
-          {satelliteView && (
-            <div className="info w-25 align-self-start m-3 pb-4">
-              <Card className=" w-25 rounded ph-color contain">
-                <div className="d-grid">
-                  <Button
-                    className="street-view rounded w-100"
-                    onClick={(e) => setSatellite(!satelliteView)}
-                  />
-                </div>
-              </Card>
-            </div>
-          )}
+          <div className="info w-25 align-self-start m-3 pb-4">
+            <Card className=" w-25 rounded ph-color contain">
+              <div className="d-grid">
+                <Button
+                  className="view-button rounded w-100"
+                  onClick={() => setSatellite(!satelliteView)}
+                />
+              </div>
+            </Card>
+          </div>
         </>
       )}
       {!perimeter && track && (
@@ -169,77 +147,30 @@ function Map({ interactive, perimeter, setPerimeters, setActive, track }) {
             className={interactive ? '' : 'map-disabled'}
             zoomControl={interactive}
           >
-            {!satelliteView && (
-              <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-                url={tileLayerData}
-              />
-            )}
-            {satelliteView && (
-              <TileLayer
-                attribution={
-                  'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
-                }
-                url={tileLayerData}
-              />
-            )}
+            <TileLayer url={tileLayerData.url} attribution={tileLayerData.attribution} />
             <Tracker />
           </MapContainer>
+          {/* save perimeter button */}
           <div className="info w-25 mb-5 d-flex justify-content-center align-content-center">
             <Card className=" w-100 rounded ph-color">
               <div className="d-grid gap-3">
-                {!tracking && (
-                  <Button className="btn-color rounded w-100" onClick={startTracking}>
-                    Start Tracking
-                  </Button>
-                )}
-                {tracking && (
-                  <Button className="btn-color rounded w-100" onClick={stopTracking}>
-                    Stop
-                  </Button>
-                )}
+                <Button className="btn-color rounded w-100" onClick={toggleTracking(!tracking)}>
+                  {tracking ? 'Stop tracking' : 'Start Tracking'}
+                </Button>
               </div>
             </Card>
           </div>
           {/* satelite view button */}
-          {!satelliteView && (
-            <div className="info w-25 align-self-start m-3 pb-4">
-              <Card className=" w-25 rounded ph-color">
-                <div className="d-grid contain">
-                  <Button
-                    className="satelite-view rounded w-100"
-                    onClick={(e) => setSatelliteView(!satelliteView)}
-                  />
-                </div>
-              </Card>
-            </div>
-          )}
-          {/* satelite view button */}
-          {!satelliteView && (
-            <div className="info w-25 align-self-start m-3 pb-4">
-              <Card className=" w-25 rounded ph-color contain">
-                <div className="d-grid contain">
-                  <Button
-                    className="satelite-view rounded w-100"
-                    onClick={(e) => setSatelliteView(!satelliteView)}
-                  />
-                </div>
-              </Card>
-            </div>
-          )}
-          {/* street view button */}
-          {satelliteView && (
-            <div className="info w-25 align-self-start m-3 pb-4">
-              <Card className=" w-25 rounded ph-color contain">
-                <div className="d-grid">
-                  <Button
-                    className="street-view rounded w-100"
-                    onClick={(e) => setSatelliteView(!satelliteView)}
-                  />
-                </div>
-              </Card>
-            </div>
-          )}
+          <div className="info w-25 align-self-start m-3 pb-4">
+            <Card className=" w-25 rounded ph-color contain">
+              <div className="d-grid contain">
+                <Button
+                  className="view-button rounded w-100"
+                  onClick={() => setSatellite(!satelliteView)}
+                />
+              </div>
+            </Card>
+          </div>
         </section>
       )}
       {!perimeter && !track && (
@@ -251,48 +182,20 @@ function Map({ interactive, perimeter, setPerimeters, setActive, track }) {
             className={interactive ? '' : 'map-disabled'}
             zoomControl={interactive}
           >
-            {!satelliteView && (
-              <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-                url={tileLayerData}
-              />
-            )}
-            {satelliteView && (
-              <TileLayer
-                attribution={
-                  'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
-                }
-                url={tileLayerData}
-              />
-            )}
+            <TileLayer url={tileLayerData.url} attribution={tileLayerData.attribution} />
             <Tracker />
           </MapContainer>
           {/* satelite view button */}
-          {!satelliteView && (
-            <div className="info w-25 align-self-start m-3 pb-4">
-              <Card className=" w-25 rounded ph-color contain">
-                <div className="d-grid">
-                  <Button
-                    className="satelite-view rounded w-100"
-                    onClick={(e) => setSatellite(!satelliteView)}
-                  />
-                </div>
-              </Card>
-            </div>
-          )}
-          {/* street view button */}
-          {satelliteView && (
-            <div className="info w-25 align-self-start m-3 pb-4">
-              <Card className=" w-25 rounded ph-color contain">
-                <div className="d-grid">
-                  <Button
-                    className="street-view rounded w-100"
-                    onClick={(e) => setSatellite(!satelliteView)}
-                  />
-                </div>
-              </Card>
-            </div>
-          )}
+          <div className="info w-25 align-self-start m-3 pb-4">
+            <Card className=" w-25 rounded ph-color contain">
+              <div className="d-grid">
+                <Button
+                  className="view-button rounded w-100"
+                  onClick={() => setSatellite(!satelliteView)}
+                />
+              </div>
+            </Card>
+          </div>
         </section>
       )}
     </>
