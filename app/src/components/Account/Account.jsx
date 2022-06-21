2f9e0edaffdd01a801c19e6b805d69ae9b7347a8
firewallfail <firewallfail@users.noreply.github.com>
2022-06-21 01:56:18 +0000

    Prettified Code!

--- app/src/components/Account/Account.jsx
+++ app/src/components/Account/Account.jsx
@@ -16,7 +16,7 @@ function Account() {
       phone: e.target[1].value,
       email: e.target[2].value,
       password: e.target[3].value,
-    }
+    };
     const data = await axios.patch(`http://localhost:${VITE_PORT_EXPRESS}/api/user`, user);
     if (data.error) {
       return setUpdate('An Error Occured');
@@ -44,46 +44,46 @@ function Account() {
       <div className="info">
         <h2 className="centered">Account Info</h2>
         <Card className="m-3 p-3 rounded w-25 ph-color w-100">
-              <Form className="d-flex flex-column justify-content-center " onSubmit={updateUser}>
-                <Form.Group className="form-group my-2 p-3">
-                  <Form.Control
-                    className="form-control bg-transparent text-white ph-color"
-                    type="text"
-                    id="name"
-                    defaultValue={users.name}
-                  />
-                </Form.Group>
-                <Form.Group className="form-group my-2 pb-3 px-3">
-                  <Form.Control
-                    className="form-control form-control bg-transparent text-white ph-color"
-                    type="text"
-                    id="phone"
-                    defaultValue={users.phone}
-                  />
-                </Form.Group>
-                <Form.Group className="form-group my-2 pb-3 px-3">
-                  <Form.Control
-                    className="form-control bg-transparent text-white ph-color"
-                    type="email"
-                    id="email"
-                    defaultValue={users.email}
-                  />
-                </Form.Group>
-                <Form.Group className="form-group my-2 pb-3 px-3">
-                  <Form.Control
-                    className="form-control bg-transparent text-white ph-color"
-                    type="password"
-                    id="password"
-                    defaultValue={users.password}
-                  />
-                </Form.Group>
-                <Form.Group className=" my-2 px-3">
-                  <Button type="submit" className="btn-color rounded w-100">
-                    {update}
-                  </Button>
-                </Form.Group>
-              </Form>
-            </Card>          
+          <Form className="d-flex flex-column justify-content-center " onSubmit={updateUser}>
+            <Form.Group className="form-group my-2 p-3">
+              <Form.Control
+                className="form-control bg-transparent text-white ph-color"
+                type="text"
+                id="name"
+                defaultValue={users.name}
+              />
+            </Form.Group>
+            <Form.Group className="form-group my-2 pb-3 px-3">
+              <Form.Control
+                className="form-control form-control bg-transparent text-white ph-color"
+                type="text"
+                id="phone"
+                defaultValue={users.phone}
+              />
+            </Form.Group>
+            <Form.Group className="form-group my-2 pb-3 px-3">
+              <Form.Control
+                className="form-control bg-transparent text-white ph-color"
+                type="email"
+                id="email"
+                defaultValue={users.email}
+              />
+            </Form.Group>
+            <Form.Group className="form-group my-2 pb-3 px-3">
+              <Form.Control
+                className="form-control bg-transparent text-white ph-color"
+                type="password"
+                id="password"
+                defaultValue={users.password}
+              />
+            </Form.Group>
+            <Form.Group className=" my-2 px-3">
+              <Button type="submit" className="btn-color rounded w-100">
+                {update}
+              </Button>
+            </Form.Group>
+          </Form>
+        </Card>
       </div>
     </section>
   );
