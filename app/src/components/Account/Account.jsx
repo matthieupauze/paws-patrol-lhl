3d588742bc60d8ec148521e4bfd6bdef53ce4e30
firewallfail <firewall.fail@hotmail.com>
2022-06-20 20:36:17 -0400

    Progress

--- app/src/components/Account/Account.jsx
+++ app/src/components/Account/Account.jsx
@@ -1,32 +1,38 @@
 import { useEffect, useState } from 'react';
-import { Button, Card } from 'react-bootstrap';
+import { Form, Button, Card, ButtonGroup, ToggleButton } from 'react-bootstrap';
 import axios from 'axios';
 import Map from '../Map';
 
 const { VITE_PORT_EXPRESS } = import.meta.env;
 
 function Account() {
-  const [users, setUsers] = useState([
+  const [users, setUsers] = useState(
     {
+      id: 1,
       name: 'Robbie Prokop',
       phone: '1-234-567-8909',
       email: 'support@paw-patrol.com',
       password: '123456789',
     },
-  ]);
+  );
+
+  const updateUser = (e) => {
+    e.preventDefault();
+    console.log("hello");
+  };
 
   useEffect(() => {
     const loadUsers = async () => {
       const { data } = await axios.get(`http://localhost:${VITE_PORT_EXPRESS}/api/user`);
       console.log(data);
       setUsers({
+        id: data.id,
         name: data.name,
         phone: data.phone,
         email: data.email,
         password: data.password,
       });
     };
-    console.log(users);
     loadUsers();
   }, []);
   return (
@@ -34,23 +40,46 @@ function Account() {
       <Map interactive={false} />
       <div className="info">
         <h2 className="centered">Account Info</h2>
-        <Card className="text-center rounded form">
-          <Card.Body>
-            <Card.Text>Name</Card.Text>
-            <Card.Title className="border-bottom border-dark pb-1 text-white">
-              {users.name}
-            </Card.Title>
-            <Card.Text>Phone</Card.Text>
-            <Card.Title className="border-bottom border-dark pb-1 ">{users.phone}</Card.Title>
-            <Card.Text>Email</Card.Text>
-            <Card.Title className="border-bottom border-dark pb-1">{users.email}</Card.Title>
-            <Card.Text>Password</Card.Text>
-            <Card.Title className="border-bottom border-dark pb-1">{users.password}</Card.Title>
-          </Card.Body>
-        </Card>
-        <div className="d-grid mt-3">
-          <Button className="btn-color rounded">Edit</Button>
-        </div>
+        <Card className="m-3 p-3 rounded w-25 ph-color w-100">
+              <Form className="d-flex flex-column justify-content-center " onSubmit={updateUser}>
+                <Form.Group className="form-group my-2 p-3">
+                  <Form.Control
+                    className="form-control bg-transparent text-white ph-color"
+                    type="text"
+                    id="name"
+                    placeholder={users.name}
+                  />
+                </Form.Group>
+                <Form.Group className="form-group my-2 pb-3 px-3">
+                  <Form.Control
+                    className="form-control form-control bg-transparent text-white ph-color"
+                    type="text"
+                    id="phone"
+                    placeholder={users.phone}
+                  />
+                </Form.Group>
+                <Form.Group className="form-group my-2 pb-3 px-3">
+                  <Form.Control
+                    className="form-control bg-transparent text-white ph-color"
+                    type="text"
+                    id="email"
+                    placeholder={users.email}
+                  />
+                </Form.Group>
+                <Form.Group className="form-group my-2 pb-3 px-3">
+                  <Form.Control
+                    className="form-control bg-transparent text-white ph-color"
+                    type="password"
+                    id="password"
+                  />
+                </Form.Group>
+                <Form.Group className=" my-2 px-3">
+                  <Button type="submit" className="btn-color rounded w-100">
+                    Add Device
+                  </Button>
+                </Form.Group>
+              </Form>
+            </Card>
       </div>
     </section>
   );
