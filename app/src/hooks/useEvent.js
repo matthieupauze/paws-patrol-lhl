dac0dfb1c70f98183b1e7d6f1395e99b1d6c2240
Matthieu Pauze <matt.pauze@gmail.com>
2022-06-20 20:58:13 -0400

    Replace event based system with polling for demo

--- app/src/hooks/useEvent.js
+++ app/src/hooks/useEvent.js
@@ -1,12 +1,12 @@
-import { io } from 'socket.io-client';
+// import { io } from 'socket.io-client';
 const { VITE_PORT_EXPRESS } = import.meta.env;
-const socket = io(`ws://localhost:${VITE_PORT_EXPRESS}`);
 
 const useEvent = (updatePosition, foundPosition, defaultPosition) => {
-  socket.on('gps-update', (coords) => {
-    console.log('received new coordinates', coords);
-    updatePosition({ coords, foundPosition });
-  });
+  // const socket = io(`ws://localhost:${VITE_PORT_EXPRESS}`);
+  // socket.on('gps-update', (coords) => {
+  //   console.log('received new coordinates', coords);
+  //   updatePosition({ coords, foundPosition });
+  // });
 };
 
 export default useEvent;
